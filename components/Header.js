2de6765eb71ff43cb7130a5f8ceaaba51a3322cb
Abdul Samad <abdulsamad.laghari1@gmail.com>
2023-05-22 21:19:05 +0500

    Note taking app initial commit

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className='py-4 px-4 bg-green-600 text-white'>
+      <p className='text-center text-lg font-bold'>Simple Note Taking App</p>
+    </div>
+  );
+};
+
+export default Header;
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
-import '@/styles/globals.css'
+import Header from "@/components/Header";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import "@/styles/globals.css";
+
+export default function App({Component, pageProps}) {
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  );
 }
